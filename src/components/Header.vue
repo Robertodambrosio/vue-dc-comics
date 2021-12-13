7b6c2fad2610f2ce7f83087e769e8ac8da84ccc3
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-12-13 14:47:42 +0100

    init components

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+     <div class="logo">
+         <img src="../assets/dc-logo.png" alt="dc-logo">
+     </div> 
+     <nav>
+         <ul>
+             <li><a href=""></a></li>
+             <li><a href=""></a></li>
+             <li><a href=""></a></li>
+             <li><a href=""></a></li>
+             <li><a href=""></a></li>
+             <li><a href=""></a></li>
+             <li><a href=""></a></li>
+             <li><a href=""></a></li>
+         </ul>            
+     </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
