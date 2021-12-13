9b8046b729607a2623201aabd94655ad126825a0
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-12-13 15:38:16 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,52 @@
 <template>
-  <header>
+  <div class="container header">
      <div class="logo">
          <img src="../assets/dc-logo.png" alt="dc-logo">
      </div> 
      <nav>
          <ul>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
+             <li><a href="">link</a></li>
+             <li><a href="">link</a></li>
+             <li><a href="">link</a></li>
+             <li><a href="">link</a></li>
+             <li><a href="">link</a></li>
+             <li><a href="">link</a></li>
+             <li><a href="">link</a></li>
+             <li><a href="">link</a></li>
          </ul>            
      </nav>
-  </header>
+  </div>
 </template>
 
 <script>
 export default {
-
+ name: 'Header'
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 80px;
 
+        img {
+            width: 60px;
+        }
+
+        ul {
+            display: flex;
+            justify-content: space-around;
+            list-style: none;
+
+            li {
+                padding: 20px;
+
+                a {
+                    text-decoration: none;
+                    text-transform: uppercase;                }
+            }
+        }
+    }
 </style>
