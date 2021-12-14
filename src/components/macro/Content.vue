b289e865342c9168752d581e925e398a46d90d56
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-12-14 16:19:17 +0100

    ticket

--- src/components/macro/Content.vue
+++ src/components/macro/Content.vue
@@ -1,17 +1,23 @@
 <template>
   <main>
       <Hero/>
-
+      <div class="container">
+          <Comics/>
+      </div>    
   </main>
 </template>
 
 <script>
 import Hero from '../macro/section/Hero.vue'
+import Comics from '../macro/section/Comics.vue'
+
+
 
 export default {
 name: 'Content',
 components: {
     Hero,
+    Comics
 }
 }
 </script>
---
+++ src/components/macro/section/SingleComic.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="comic">
+        <img :src="element.thumb" :alt="element.type">
+        <p>{{element.series}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SingleComic',
+        props : {
+            comic: Object
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
